d47e693f4a81b2d8035cc0c2d60ebb1bf8b65394
tpelleterat neo <tpelleterat@NS-BDX-DEV31>
2017-09-14 12:39:06 +0200

    Add ViewModelContentPage

---
+++ TimFlyMobile/TimFlyMobile/Constrols/ViewModelContentPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimFlyMobile.Constrols
+{
+    public class ViewModelContentPage : ContentPage
+    {
+        public ViewModelContentPage()
+        {
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
