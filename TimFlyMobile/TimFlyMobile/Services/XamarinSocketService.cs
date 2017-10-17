38f0e8419aac16b563406d697c97e56b3969f72d
tpelleterat neo <tpelleterat@NS-BDX-DEV31>
2017-10-17 11:19:42 +0200

    Quanity and version

--- TimFlyMobile/TimFlyMobile/Services/XamarinSocketService.cs
+++ TimFlyMobile/TimFlyMobile/Services/XamarinSocketService.cs
@@ -1,11 +1,7 @@
 ﻿using Sockets.Plugin;
-using Sockets.Plugin.Abstractions;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace MobilePrototype.Services
